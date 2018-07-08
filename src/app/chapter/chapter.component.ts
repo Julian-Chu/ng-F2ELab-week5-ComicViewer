a4af82773668066e28b5a2d2f8d07aa657452ccc
JulianChu <yulang.chu@gmail.com>
2018-07-08 16:04:25 +0200

    get pages from service

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { PagesService, Page } from "./../pages.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-chapter',
-  templateUrl: './chapter.component.html',
-  styleUrls: ['./chapter.component.css']
+  selector: "app-chapter",
+  templateUrl: "./chapter.component.html",
+  styleUrls: ["./chapter.component.css"]
 })
 export class ChapterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  pages: Page[];
+  ngOnInit(): void {}
+  constructor(pagesService: PagesService) {
+    this.pages = pagesService.getPages();
   }
-
 }
---
+++ src/app/pages.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class PagesService {
+  constructor() {}
+
+  getPages(): Page[] {
+    return [
+      new Page(1, "../../assets/storyboard-1.png"),
+      new Page(2, "../../assets/storyboard-2.png"),
+      new Page(3, "../../assets/storyboard-3.png"),
+      new Page(4, "../../assets/storyboard-4.png"),
+      new Page(5, "../../assets/storyboard-5.png"),
+      new Page(6, "../../assets/storyboard-6.png"),
+      new Page(7, "../../assets/storyboard-7.png")
+    ];
+  }
+}
+
+export class Page {
+  /**
+   *
+   */
+  constructor(number: number, link: string) {
+    this.pageNumber = number;
+    this.pageImageLink = link;
+  }
+  public pageNumber: number;
+  public pageImageLink: string;
+}
