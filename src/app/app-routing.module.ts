642de2751f1658be3adfdb5c95c90d5605ae4d08
JulianChu <yulang.chu@gmail.com>
2018-07-08 15:28:08 +0200

    install chapter router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ChapterComponent } from "./chapter/chapter.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "chapter", component: ChapterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
